1e600aeda08c965cbdb92bcec3157f6a93803a72
BOUDIN Eric <be714071@iutnice.unice.fr>
2018-09-17 16:45:30 +0200

    Create new classes

---
+++ src/main/java/fr/iut/Prix.java
@@ -0,0 +1,9 @@
+package fr.iut;
+
+public class Prix {
+
+    public static double calculerPanier(Double prixHT, int quantite){
+        return prixHT*quantite;
+    }
+
+}
--- src/test/java/fr/iut/test.java
+++ src/test/java/fr/iut/test.java
@@ -1,11 +1,20 @@
 package fr.iut;
 
 import org.junit.Test;
+import fr.iut.Prix;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class test {
     @Test
-    public void testHT(){
-
+    public void testCreerProduit(){
+        HashMap<String,Double> listeProduits = new HashMap<String,Double>();
+        listeProduits.put("Chèvre",15.0);
+        double prixHT = Prix.calculerPanier(listeProduits.get("Chèvre"),13);
+        assertTrue(prixHT==195.0);
     }
 
 }
